9f0b3b1307c41cd3e8b4987b0f0d631e4cd30eaf
mgomes-d <murillo.jogos8@hotmail.com>
2024-01-16 10:01:41 +0100

    ex04

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,6 +1,21 @@
 import sys
 
-def whatis():
-    try:
 
-    except Assertion:
+def main():
+    try:
+        if len(sys.argv) < 2:
+            return
+        assert len(sys.argv) <= 2, "more than one argument is provided"
+        value = int(sys.argv[1])
+        if value % 2:
+            print("I'm Odd.")
+        else:
+            print("I'm Even.")
+    except AssertionError as msg:
+        print("AssertionError:", msg)
+    except ValueError:
+        print("AssertionError: argument is not an integer")
+
+
+if __name__ == "__main__":
+    main()
