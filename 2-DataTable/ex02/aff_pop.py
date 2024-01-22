e64c8438f3a1ba9be28ff52b2bdabba7c713191e
mgomes-d <murillo.jogos8@hotmail.com>
2024-01-22 18:41:02 +0100

    ex02 done

--- 2-DataTable/ex02/aff_pop.py
+++ 2-DataTable/ex02/aff_pop.py
@@ -5,17 +5,27 @@ import matplotlib.pyplot as plt
 def main():
     try:
         data_frame = load("population_total.csv")
-        country_data_1 = data_frame[data_frame['country'] == 'Belgium']
-        # country_data_2 = data_frame[data_frame['country'] == 'Belgium']
-        if not country_data_1.empty:
-            years = [year for year in country_data_1.columns if year.isdigit() and 1800 <= int(year) <= 2050 and int(year) % 40 == 0]
-            
-            print(country_data_1)
-            plt.plot(country_data_1.columns[1:], country_data_1.iloc[0, 1:])
-            plt.xticks(years)
+        belgium_data = data_frame[data_frame['country'] == 'Belgium'].iloc[:, 1:]
+        france_data = data_frame[data_frame['country'] == 'France'].iloc[:, 1:]
+        if not belgium_data.empty and not france_data.empty:
+
+            belgium_popu = [(float(popu[:-1]) * 1e6) for popu, year in zip(belgium_data.values.flatten(), belgium_data.columns.astype(int)) if year <= 2050]
+            france_popu = [(float(popu[:-1]) * 1e6) for popu, year in zip(france_data.values.flatten(), france_data.columns.astype(int)) if year <= 2050]
+
+            years = [year for year in france_data.columns.astype(int) if year <= 2050]
+            plt.plot(years, belgium_popu, label='Belgium')
+            plt.plot(years, france_popu, label='France')
+
+            years_x = [year for year in years if 1800 <= year <= 2050 and year % 40 == 0]
+            plt.xticks(years_x)
+            mpopu = max(max(belgium_popu), max(france_popu))
+            ypopu = [i * 1e7 for i in range(int(mpopu / 1e7) + 1) if (i * 1e7) % 2e7 == 0 and i != 0]
+            plt.yticks(ypopu, ["{:,.0f}M".format(popu / 1e6) for popu in ypopu])
+
             plt.title(f'Population Projections')
             plt.xlabel('Year')
             plt.ylabel('Population')
+            plt.legend(loc="lower right")
             plt.show()
         else:
             print(f"Data not available for {'Belgium'}")
