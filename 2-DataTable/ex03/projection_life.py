2a228ec48e988e0a2e545d54fa724a4b6454b001
mgomes-d <murillo.jogos8@hotmail.com>
2024-01-23 17:41:58 +0100

    finish ex 03

--- 2-DataTable/ex03/projection_life.py
+++ 2-DataTable/ex03/projection_life.py
@@ -1,5 +1,6 @@
 from load_csv import load
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def main():
@@ -8,14 +9,15 @@ def main():
 
     life_expenctancy_1900 = life_expenctancy["1900"]
     income_person_1900 = income_person["1900"]
-    plt.scatter(income_person_1900, life_expenctancy_1900, marker='o')
-    x_ticks = []
+    plt.scatter(income_person_1900, life_expenctancy_1900)
+    x_ticks = np.array([300.0, 1000.0, 10000.0])
+    plt.xlabel("Gross domestic product")
+    plt.ylabel("Life Expectancy")
+    plt.title("1900")
+    plt.xlim(300)
+    plt.xscale("log")
+    plt.xticks(x_ticks, ["{:,.0f}k".format(pers / 1e3) if pers >= 1000 else "{:,.0f}".format(pers) for pers in x_ticks])
     plt.show()
-    # print(income_person_1900)
-    # print(life_expenctancy_1900)
-    # print(life_expenctancy)
-    # print(income_person)
-
 
 
 if __name__ == "__main__":
