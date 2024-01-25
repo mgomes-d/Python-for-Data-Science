fca31c22a506426b28e075c9b4f5b90d1aa868c0
mgomes-d <murillo.jogos8@hotmail.com>
2024-01-25 17:23:49 +0100

    ex00 done

--- 4-Dod/ex00/statistics.py
+++ 4-Dod/ex00/statistics.py
@@ -1,72 +1,69 @@
 
 def ft_mean(array):
-    try:
-        result = 0
-        index = 0
-        for number in array:
-            result += number
-            index += 1
-        result /= index
-        return result
-    except:
-        print("ERROR")
+    result = 0
+    index = 0
+    for number in array:
+        result += number
+        index += 1
+    result /= index
+    return result
 
 def ft_median(array):
-    try:
-        array.sort()
-        index = int(len(array) / 2)
-        result = array[index]
-        return result
-    except:
-        print("ERROR")
+    array.sort()
+    index = int(len(array) / 2)
+    result = array[index]
+    return result
 
 def ft_quartile(array):
-    try:
-        array.sort()
-        index_25 = int(len(array) * 0.25)
-        index_75 = int(len(array) * 0.75)
-        result = []
-        result.append(float(array[index_25]))
-        result.append(float(array[index_75]))
-        return result
-    except:
-        print("ERROR")
+    array.sort()
+    index_25 = int(len(array) * 0.25)
+    index_75 = int(len(array) * 0.75)
+    result = []
+    result.append(float(array[index_25]))
+    result.append(float(array[index_75]))
+    return result
 
 def ft_standard(array):
-    try:
-        mean = ft_mean(array)
-        result = 0
-        for i in range(len(array)):
-            result += (array[i] - mean) **2
-        result = (result / len(array))**0.5
-        return result
-    except:
-        print("ERROR")
+    mean = ft_mean(array)
+    result = 0
+    for i in range(len(array)):
+        result += (array[i] - mean) **2
+    result = (result / len(array))**0.5
+    return result
 
 def ft_variance(array):
-    try:
-        result = ft_standard(array) ** 2
-        return result
-    except:
-        print("ERROR")
+    result = ft_standard(array) ** 2
+    return result
 
 def ft_statistics(*args: any, **kwargs: any) -> None:
     numbers = []
     for value in args:
         numbers.append(value)
     for key, value in kwargs.items():
-        if value == "mean":
-            result = 
-            print(f'mean : {ft_mean(numbers)}')
-        elif value == "median":
-            result = 
-            print(f'median : {ft_median(numbers)}')
-        elif value == "quartile":
-            result = 
-            print(f'quartile : {ft_quartile(numbers)}')
-        elif value == "var":
-            result = 
-            print(f'var : {ft_variance(numbers)}')
-        elif value == "std":
-            result = 
-            print(f'std : {ft_standard(numbers)}')
+        try:
+            if value == "mean":
+                result = ft_mean(numbers)
+                if result:
+                    print(f'mean : {result}')
+            elif value == "median":
+                result = ft_median(numbers)
+                if result:
+                    print(f'median : {result}')
+            elif value == "quartile":
+                result = ft_quartile(numbers)
+                if result:
+                    print(f'quartile : {result}')
+            elif value == "var":
+                result = ft_variance(numbers)
+                if result:
+                    print(f'var : {result}')
+            elif value == "std":
+                result = ft_standard(numbers)
+                if result:
+                    print(f'std : {result}')
+        except IndexError:
+            print("ERROR")
+        except ZeroDivisionError:
+            print("ERROR")
+
+
