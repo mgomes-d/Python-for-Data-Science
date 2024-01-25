af62037408a9ab71c5f4555543d7fa555bf901b6
mgomes-d <murillo.jogos8@hotmail.com>
2024-01-25 18:03:29 +0100

    4-dod-ex01

---
+++ 4-Dod/ex01/in_out.py
@@ -0,0 +1,10 @@
+def square(x: int | float) -> int | float:
+    return x * x
+def pow(x: int | float) -> int | float:
+    return x**x
+def outer(x: int | float, function) -> object:
+    def inner() -> float:
+        nonlocal x
+        x = function(x)
+        return x
+    return inner        
