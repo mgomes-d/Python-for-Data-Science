8a3b14db014a1631ddf10d50f99a74af0fa0e141
mgomes-d <murillo.jogos8@hotmail.com>
2024-01-26 17:18:36 +0100

    4-dod-ex02 done

--- 4-Dod/ex02/callLimit.py
+++ 4-Dod/ex02/callLimit.py
@@ -1,6 +1,15 @@
 def callLimit(limit: int):
     count = 0
     def callLimiter(function):
-        def limit_function(*args: Any, **kwds: Any):
-            #your code here
+        def limit_function(*args: any, **kwds: any):
+            nonlocal count
+            try:
+                count += 1
+                assert count <= limit, f"function {function} call too many times"
+                return function(*args, **kwds)
+            except AssertionError as msg:
+                print("Error:", msg)
+
+        return limit_function
+    return callLimiter
             
